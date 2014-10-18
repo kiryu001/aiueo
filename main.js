0507d7ddc264e437afe93b4ea638526664fcff4f
Shuhei Hiya <syuhei176@gmail.com>
2014-10-18 16:33:58 +0900

    add images

--- main.js
+++ main.js
@@ -2,13 +2,13 @@
     setTimeout(function() {
         $("#logo").hide();
         start()
-    }, 2000);
+    }, 1200);
     function start() {
     var milkcocoa = new MilkCocoa("https://io-li1eaqdxg.mlkcca.com");
     var hash = escapeHTML(location.hash.substr(1));
     var dataStore = milkcocoa.dataStore("kobito").child(hash);
     $("#kobito").offset({left : 0, top :0});
-    var url = "https://dl.dropboxusercontent.com/u/6931359/shibuya1018/images/aiueo_";
+    var url = "images/aiueo_";
     var aiueo_map = {
         "あ" : 1,
         "い" : 2,
@@ -20,6 +20,42 @@
         "く" : 8,
         "け" : 9,
         "こ" : 10,
+        "さ" : 11,
+        "し" : 12,
+        "す" : 13,
+        "せ" : 14,
+        "そ" : 15,
+        "た" : 16,
+        "ち" : 17,
+        "つ" : 18,
+        "て" : 19,
+        "と" : 20,
+        "な" : 21,
+        "に" : 22,
+        "ぬ" : 23,
+        "ね" : 24,
+        "の" : 25,
+        "は" : 26,
+        "ひ" : 27,
+        "ふ" : 28,
+        "へ" : 29,
+        "ほ" : 30,
+        "ま" : 31,
+        "み" : 32,
+        "む" : 33,
+        "め" : 34,
+        "も" : 35,
+        "や" : 36,
+        "ゆ" : 37,
+        "よ" : 38,
+        "ら" : 39,
+        "り" : 40,
+        "る" : 41,
+        "れ" : 42,
+        "ろ" : 43,
+        "わ" : 44,
+        "を" : 45,
+        "ん" : 46
     };
 
 function AiueoElement(_x, _y, _text) {
@@ -45,12 +81,19 @@ function set_element(x, y, text) {
     }, 1000);
     setTimeout(function() {
         var ae = new AiueoElement(tx, ty, text);
+        $("#kobito").animate({
+            left : -95,
+            top : 0
+        }, 1000);
     }, 1000);
 }
 
 function get_aiueo(text) {
-    return text.split("").map(function(character){
-        return url + aiueo_map[character] + ".png";
+    return text.split("").map(function(c){
+        if(aiueo_map[c]) return aiueo_map[c];
+        return 0;
+    }).map(function(character){
+        return url + character + ".png";
     }).map(function(e){
         return '<img width="120" src="'+e+'"></img>'
     }).join("");
