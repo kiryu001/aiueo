672c8d936e4185ce6a4ca5a903977565864079c7
Shuhei Hiya <syuhei176@gmail.com>
2014-10-18 16:16:27 +0900

    Update

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 (function(){
-	
-var milkcocoa = new MilkCocoa("https://io-li1eaqdxg.mlkcca.com");
-var dataStore = milkcocoa.dataStore("kobito");
-$("#kobito").offset({left : 0, top :0});
-var url = "https://dl.dropboxusercontent.com/u/6931359/shibuya1018/images/aiueo_";
-var aiueo_map = {
-    "あ" : 1,
-    "い" : 2,
-    "う" : 3,
-    "え" : 4,
-    "お" : 5,
-    "か" : 6,
-    "き" : 7,
-    "く" : 8,
-    "け" : 9,
-    "こ" : 10,
-};
+    setTimeout(function() {
+        $("#logo").hide();
+        start()
+    }, 2000);
+    function start() {
+    var milkcocoa = new MilkCocoa("https://io-li1eaqdxg.mlkcca.com");
+    var hash = escapeHTML(location.hash.substr(1));
+    var dataStore = milkcocoa.dataStore("kobito").child(hash);
+    $("#kobito").offset({left : 0, top :0});
+    var url = "https://dl.dropboxusercontent.com/u/6931359/shibuya1018/images/aiueo_";
+    var aiueo_map = {
+        "あ" : 1,
+        "い" : 2,
+        "う" : 3,
+        "え" : 4,
+        "お" : 5,
+        "か" : 6,
+        "き" : 7,
+        "く" : 8,
+        "け" : 9,
+        "こ" : 10,
+    };
 
 function AiueoElement(_x, _y, _text) {
     var x = _x;
@@ -30,6 +35,10 @@ function AiueoElement(_x, _y, _text) {
 function set_element(x, y, text) {
     var tx = Number(x)-60;
     var ty = Number(y)-60;
+    $("#kobito").offset({
+        left : -100,
+        top : 0
+    });
     $("#kobito").animate({
         left : tx,
         top : ty
@@ -56,7 +65,18 @@ $(window).mousedown(function(e){
     });
 });
 
+dataStore.query({}).done(function(elems) {
+    elems.map(function(e) {
+        set_element(e.x, e.y, e.text);
+    });
+});
+
 dataStore.on("push", function(e){
     set_element(e.value.x, e.value.y, e.value.text);
 });
+
+    function escapeHTML(val) {
+        return $('<div>').text(val).html();
+    };    }
+
 }())
